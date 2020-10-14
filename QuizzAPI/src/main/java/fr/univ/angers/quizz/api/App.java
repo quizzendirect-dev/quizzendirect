aaf4cb679bee21bb244c455dda681d0dbf78fb6c
Angélique Denécheau <angelique.denecheau@etud.univ-angers.fr>
2020-10-14 15:17:14 +0200

    Merge model sur api

--- QuizzAPI/src/main/java/fr/univ/angers/quizz/api/App.java
+++ QuizzAPI/src/main/java/fr/univ/angers/quizz/api/App.java
@@ -1,6 +1,10 @@
 package fr.univ.angers.quizz.api;
 
 
+import fr.univ.angers.quizz.api.model.Professeur;
+import fr.univ.angers.quizz.api.repository.ProfesseurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,13 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App 
+public class App
 {
-    
+    @Autowired
+    private ProfesseurRepository professeurRepository;
     
     public static void main( String[] args )
     {
-        
+        Professeur prof = new Professeur();
+        prof.setUserName("Prof1");
         SpringApplication.run(App.class, args);
     }
 }
--- QuizzAPI/src/main/java/fr/univ/angers/quizz/api/datasfetchers/QuestionDataFetcher.java
+++ QuizzAPI/src/main/java/fr/univ/angers/quizz/api/datasfetchers/QuestionDataFetcher.java
@@ -1,2 +1,24 @@
-package fr.univ.angers.quizz.api.datasfetchers;public class QuestionDataFetcher {
+package fr.univ.angers.quizz.api.datasfetchers;
+
+import fr.univ.angers.quizz.api.model.Question;
+import fr.univ.angers.quizz.api.repository.QuestionRepository;
+import graphql.schema.DataFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class QuestionDataFetcher {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public DataFetcher<List<Question>> getAllProduct(){
+        return dataFetchingEnvironment -> questionRepository.findAll();
+    }
+
+    public DataFetcher<Question> getQuestionByEnoncer(){
+        return dataFetchingEnvironment -> questionRepository.findByIntitule(dataFetchingEnvironment.getArgument("enoncer"));
+    }
 }
