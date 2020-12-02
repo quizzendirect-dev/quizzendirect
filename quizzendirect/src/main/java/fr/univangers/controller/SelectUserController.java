05156e6e053004f56a68eba8c1b6c1d5b4d3b69b
alex <alex38g@gmail.com>
2020-12-02 16:56:35 +0100

    Suppression des pages html obsolètes

--- quizzendirect/src/main/java/fr/univangers/controller/SelectUserController.java
+++ quizzendirect/src/main/java/fr/univangers/controller/SelectUserController.java
@@ -2,13 +2,12 @@
 package fr.univangers.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SelectUserController {
     @GetMapping("/SelectPage")
     public String getSelectedPage(){
-            return "SelectionPage";
+            return "home";
     }
 }
